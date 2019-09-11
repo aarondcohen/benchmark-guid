ca4bef151ef46e2ffced2c36c040db8036c80357
Aaron Cohen <aarondcohen@gmail.com>
2019-09-11 19:22:12 -0400

    Use supported benchmark suite
    - switch from matcha to benchmarkify
    - ensure tests aren't optimized away
    - update benchmark results

--- benchmark.js
+++ benchmark.js
@@ -1,3 +1,4 @@
+const Benchmarkify = require("benchmarkify");
 const {
 	Ulid: Id128Ulid,
 	UlidMonotonic: Id128UlidMonotonic,
@@ -18,121 +19,96 @@ const Uuid1345 = require('uuid-1345');
 const Uuid4 = require('uuid4');
 const UuidJs = require('uuidjs');
 
-suite('Competitors', function() {
-  set('iterations', 100000);
+let tmp;
 
-	bench('Id128.Ulid', function() {
-		Id128Ulid.generate();
-	});
-
-	bench('Id128.Ulid Canonical', function() {
-		Id128Ulid.generate().toCanonical();
-	});
-
-	bench('Id128.UlidMonotonic', function() {
-		Id128UlidMonotonic.generate();
-	});
-
-	bench('Id128.UlidMonotonic Canonical', function() {
-		Id128UlidMonotonic.generate().toCanonical();
-	});
-
-	bench('Id128.Uuid1', function() {
-		Id128Uuid1.generate();
-	});
-
-	bench('Id128.Uuid1 Canonical', function() {
-		Id128Uuid1.generate().toCanonical();
-	});
-
-	bench('Id128.Uuid4', function() {
-		Id128Uuid4.generate();
-	});
-
-	bench('Id128.Uuid4 Canonical', function() {
-		Id128Uuid4.generate().toCanonical();
-	});
-
-	bench('Id128.Uuid6', function() {
-		Id128Uuid6.generate();
-	});
-
-	bench('Id128.Uuid6 Canonical', function() {
-		Id128Uuid6.generate().toCanonical();
-	});
-
-	bench('Id128.UuidNil', function() {
-		Id128UuidNil.generate();
-	});
-
-	bench('Id128.UuidNil Canonical', function() {
-		Id128UuidNil.generate().toCanonical();
-	});
-
-	bench('Cuid', function() {
-		Cuid();
-	});
-
-	bench('Ksuid', function() {
-		Ksuid.randomSync();
-	});
-
-	bench('Nanoid', function() {
-		Nanoid();
-	});
-
-	const HEX_ALPHABET = '0123456789ABCDEF';
-	bench('Nanoid like Uuid v4', function() {
-		Nanoid(32, HEX_ALPHABET);
-	});
-
-	bench('Ulid', function() {
-		Ulid.ulid();
-	});
-
-	const ulidMonotonic = Ulid.monotonicFactory();
-	bench('Ulid Monotonic', function() {
-		ulidMonotonic();
-	});
-
-	bench('Uuid1345.v1', function() {
-		Uuid1345.v1();
-	});
-
-	bench('Uuid1345.v4', function() {
-		Uuid1345.v4();
-	});
-
-	bench('Uuidv1', function() {
-		Uuidv1();
-	});
-
-	bench('Uuidv4', function() {
-		Uuidv4();
-	});
-
-	const uuidV6 = Uuidv6.v6setup({ disableRandom: true });
-	bench('Uuidv6', function() {
-		uuidV6();
-	});
-
-	bench('UuidRandom', function() {
-		UuidRandom();
-	});
-
-	bench('Uuid4', function() {
-		Uuid4();
-	});
-
-	bench('UuidJs', function() {
-		UuidJs.generate();
-	});
-
-	bench('UuidJs v4', function() {
-		UuidJs.genV4();
-	});
-
-	bench('UuidJs v4 Canonical', function() {
-		UuidJs.genV4().toString();
-	});
+const suite = new Benchmarkify().createSuite('Competitors');
+suite.add('Id128.Ulid', function() {
+	tmp = Id128Ulid.generate();
 });
+suite.add('Id128.Ulid Canonical', function() {
+	tmp = Id128Ulid.generate().toCanonical();
+});
+suite.add('Id128.UlidMonotonic', function() {
+	tmp = Id128UlidMonotonic.generate();
+});
+suite.add('Id128.UlidMonotonic Canonical', function() {
+	tmp = Id128UlidMonotonic.generate().toCanonical();
+});
+suite.add('Id128.Uuid1', function() {
+	tmp = Id128Uuid1.generate();
+});
+suite.add('Id128.Uuid1 Canonical', function() {
+	tmp = Id128Uuid1.generate().toCanonical();
+});
+suite.add('Id128.Uuid4', function() {
+	tmp = Id128Uuid4.generate();
+});
+suite.add('Id128.Uuid4 Canonical', function() {
+	tmp = Id128Uuid4.generate().toCanonical();
+});
+suite.add('Id128.Uuid6', function() {
+	tmp = Id128Uuid6.generate();
+});
+suite.add('Id128.Uuid6 Canonical', function() {
+	tmp = Id128Uuid6.generate().toCanonical();
+});
+suite.add('Id128.UuidNil', function() {
+	tmp = Id128UuidNil.generate();
+});
+suite.add('Id128.UuidNil Canonical', function() {
+	tmp = Id128UuidNil.generate().toCanonical();
+});
+suite.add('Cuid', function() {
+	tmp = Cuid();
+});
+suite.add('Ksuid', function() {
+	tmp = Ksuid.randomSync();
+});
+suite.add('Nanoid', function() {
+	tmp = Nanoid();
+});
+const HEX_ALPHABET = '0123456789ABCDEF';
+suite.add('Nanoid like Uuid v4', function() {
+	tmp = Nanoid(32, HEX_ALPHABET);
+});
+suite.add('Ulid', function() {
+	tmp = Ulid.ulid();
+});
+const ulidMonotonic = Ulid.monotonicFactory();
+suite.add('Ulid Monotonic', function() {
+	tmp = ulidMonotonic();
+});
+suite.add('Uuid1345.v1', function() {
+	tmp = Uuid1345.v1();
+});
+suite.add('Uuid1345.v4', function() {
+	tmp = Uuid1345.v4();
+});
+suite.add('Uuidv1', function() {
+	tmp = Uuidv1();
+});
+suite.add('Uuidv4', function() {
+	tmp = Uuidv4();
+});
+const uuidV6 = Uuidv6.v6setup({ disableRandom: true });
+suite.add('Uuidv6', function() {
+	tmp = uuidV6();
+});
+suite.add('UuidRandom', function() {
+	tmp = UuidRandom();
+});
+suite.add('Uuid4', function() {
+	tmp = Uuid4();
+});
+suite.add('UuidJs', function() {
+	tmp = UuidJs.generate();
+});
+suite.add('UuidJs v4', function() {
+	tmp = UuidJs.genV4();
+});
+suite.add('UuidJs v4 Canonical', function() {
+	tmp = UuidJs.genV4().toString();
+});
+suite.run();
+
+if (! tmp) throw Error('Failed Invariant: ID');
